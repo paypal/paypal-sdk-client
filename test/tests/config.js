93f15fbda69104ffd9bb1d1cef9f62c051a55224
Blade Barringer <blade.barringer@getbraintree.com>
2018-03-28 17:30:23 +0000

    Fix failing build

--- test/tests/config.js
+++ test/tests/config.js
@@ -1,7 +1,14 @@
 /* @flow */
 
+import assert from 'assert'; // eslint-disable-line import/no-nodejs-modules
+
+/* eslint-disable flowtype/no-weak-types */
+declare var describe: any;
+declare var it: any;
+/* eslint-enable */
+
 describe(`happy cases`, () => {
     it('should successfully create a client instance', () => {
-        // pass
+        assert.ok(true);
     });
 });
