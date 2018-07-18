ba24d5c2211c2f2d7905e2749c4e7fbdb71f8566
Daniel Brain <dbrain@paypal.com>
2018-07-18 11:51:07 -0700

    Add sdk globals

--- src/declarations.js
+++ src/declarations.js
@@ -4,3 +4,10 @@ import type { SDKGlobalType } from './types';
 
 declare var __TEST__ : boolean;
 declare var __sdk__ : SDKGlobalType | void
+
+declare var __ENV__ : string;
+declare var __CLIENT_ID__ : string;
+declare var __MERCHANT_ID__ : string;
+declare var __LOCALE__ : { COUNTRY : string, LANG : string };
+declare var __DEBUG__ : boolean;
+declare var __STAGE__ : string;
