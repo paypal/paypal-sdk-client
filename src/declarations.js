89b8e84205ccda6e69bd791682f87c72ec5af855
Daniel Brain <dbrain@paypal.com>
2018-08-06 15:55:00 -0700

    Add country and language constants

--- src/declarations.js
+++ src/declarations.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import type { SDKGlobalType } from './types';
+import { COUNTRY, LANG } from './constants';
 
 declare var __TEST__ : boolean;
 declare var __sdk__ : SDKGlobalType | void
@@ -8,6 +9,6 @@ declare var __sdk__ : SDKGlobalType | void
 declare var __ENV__ : string;
 declare var __CLIENT_ID__ : string;
 declare var __MERCHANT_ID__ : string;
-declare var __LOCALE__ : { __COUNTRY__ : string, __LANG__ : string };
+declare var __LOCALE__: { __COUNTRY__ : $Values<typeof COUNTRY>, __LANG__ : $Values<typeof LANG> };
 declare var __DEBUG__ : boolean;
 declare var __STAGE__ : string;
