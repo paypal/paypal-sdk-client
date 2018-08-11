b4da3c78833eae23f5655f905cd10ac617afb4f6
Daniel Brain <dbrain@paypal.com>
2018-08-10 17:51:20 -0700

    Use belter global

--- src/global.js
+++ src/global.js
@@ -1,18 +1,22 @@
 /* @flow */
 
+import { getGlobal as getGlob } from 'belter/src';
+
 import { GLOBAL_KEY } from './constants';
 
 /**
  * Get a shared global object namespaced for the client
  */
 export function getGlobal<T : mixed>(key : string, def : ?T) : T {
-    window[GLOBAL_KEY] = window[GLOBAL_KEY] || {};
-    if (window[GLOBAL_KEY].hasOwnProperty(key)) {
-        return window[GLOBAL_KEY][key];
+    let glob = getGlob();
+
+    glob[GLOBAL_KEY] = glob[GLOBAL_KEY] || {};
+    if (glob[GLOBAL_KEY].hasOwnProperty(key)) {
+        return glob[GLOBAL_KEY][key];
     }
     // $FlowFixMe
     def = def || {};
-    window[GLOBAL_KEY][key] = def;
+    glob[GLOBAL_KEY][key] = def;
     // $FlowFixMe
     return def;
 }
