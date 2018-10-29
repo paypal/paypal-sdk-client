3da881f3813a3b6fa109cda8502dbf8d17009765
Daniel Brain <dbrain@paypal.com>
2018-10-29 14:15:09 -0700

    Remove deprecated client function

--- src/interface.js
+++ src/interface.js
@@ -1,30 +1,7 @@
 /* @flow */
 
-import { SDK_SETTINGS } from 'paypal-sdk-constants/src';
-
 import { setupLogger } from './logger';
 import { getSDKScript } from './script';
-import { getEnv } from './globals';
-
-export function client({ auth = {} } : { auth : { [string] : string } } = {}) : mixed {
-
-    let msg = `paypal.client() is deprecated; please pass client token as data-client-token="xyz" in ${ getSDKScript().outerHTML }`;
-
-    if (__SANDBOX__ || __PRODUCTION__) {
-        throw new Error(msg);
-    }
-
-    // eslint-disable-next-line no-console
-    console.warn(msg);
-    
-    let clientToken = auth[getEnv()];
-    if (!clientToken) {
-        throw new Error(`Expected paypal.client() to be called with client token for ${ getEnv() }: paypal.client({ auth: { ${ getEnv() }: 'xyz' } })`);
-    }
-
-    getSDKScript().setAttribute(SDK_SETTINGS.CLIENT_TOKEN, clientToken);
-    return window.paypal;
-}
 
 export function setupClient() {
     getSDKScript();
