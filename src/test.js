0efdc68a3cc829c5310b186d38813ac2926a17a3
Daniel Brain <dbrain@paypal.com>
2019-02-18 13:51:37 -0800

    Allow passing query params into insertMockSDKScript

--- src/test.js
+++ src/test.js
@@ -1,16 +1,35 @@
 /* @flow */
 
+import { extendUrl } from 'belter/src';
+import { SDK_QUERY_KEYS } from '@paypal/sdk-constants/src';
+
 import { getHost, getPath } from './globals';
 
-export function insertMockSDKScript() {
+type MockScriptOptions = {|
+    query? : {
+        [string] : string
+    }
+|};
+
+export function insertMockSDKScript({ query = {} } : MockScriptOptions = {}) {
+
+    const src = extendUrl(`https://${ getHost() }${ getPath() }`, {
+        query: {
+            [ SDK_QUERY_KEYS.CLIENT_ID ]: 'abcxyz123',
+            ...query
+        }
+    });
+
     const script = document.createElement('script');
     script.setAttribute('id', 'test-sdk-script');
     script.setAttribute('type', 'mock/javascript');
-    script.setAttribute('src', `https://${ getHost() }${ getPath() }?client-id=abcxyz123`);
+    script.setAttribute('src', src);
 
     const body = document.body;
 
     if (body) {
         body.appendChild(script);
+    } else {
+        throw new Error(`Could not find body`);
     }
 }
