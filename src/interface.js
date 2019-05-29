59c65f79552a6b10c5cc120a5009520d7097eec5
Daniel Brain <daniel@brainblocks.io>
2019-05-28 19:35:13 -0700

    Export FUNDING constant

--- src/interface.js
+++ src/interface.js
@@ -4,6 +4,8 @@ import { setupLogger } from './logger';
 import { getSDKScript } from './script';
 import { checkForCommonErrors } from './errors';
 
+export { FUNDING } from '@paypal/sdk-constants';
+
 export function setup() {
     checkForCommonErrors();
     getSDKScript();
