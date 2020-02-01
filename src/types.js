859e76d7caac07e9af094715f03a4a4aef4e4154
Daniel Brain <dbrain@paypal.com>
2020-01-31 17:16:13 -0800

    Make funding eligibility type more granular

--- src/types.js
+++ src/types.js
@@ -2,204 +2,72 @@
 
 export const TYPES = true;
 
+export type VaultedInstrument = {|
+    id : string,
+    label : {|
+        description : string
+    |}
+|};
+
+export type BasicEligibility = {|
+    eligible : boolean,
+    branded : boolean,
+    recommended? : ?boolean
+|};
+
+export type PayPalEligibility = {|
+    eligible : boolean,
+    branded : boolean,
+    recommended? : ?boolean,
+    vaultable? : ?boolean,
+    vaultedInstruments? : $ReadOnlyArray<VaultedInstrument>
+|};
+
+export type CardVendorEligibility = {|
+    eligible : boolean,
+    vaultable? : ?boolean,
+    vaultedInstruments? : $ReadOnlyArray<VaultedInstrument>
+|};
+
+export type CardVendorsEligibility = {|
+    visa? : CardVendorEligibility,
+    mastercard? : CardVendorEligibility,
+    amex? : CardVendorEligibility,
+    discover? : CardVendorEligibility,
+    hiper? : CardVendorEligibility,
+    elo? : CardVendorEligibility,
+    jcb? : CardVendorEligibility,
+    cup? : CardVendorEligibility
+|};
+
+export type CardEligibility = {|
+    eligible : boolean,
+    branded : boolean,
+    recommended? : ?boolean,
+    vendors : CardVendorsEligibility
+|};
+
 export type FundingEligibilityType = {|
-    bancontact? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    card : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean,
-        vendors : {
-            visa? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            mastercard? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            amex? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            discover? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            hiper? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            elo? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            jcb? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            },
-            cup? : {
-                eligible : boolean,
-                vaultable? : ?boolean,
-                vaultedInstruments? : $ReadOnlyArray<{
-                    id : string,
-                    label : {
-                        description : string
-                    }
-                }>
-            }
-        }
-    },
-    credit? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    sepa? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    eps? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    giropay? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    ideal? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    mybank? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    p24? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    paypal? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean,
-        vaultable? : ?boolean,
-        vaultedInstruments? : $ReadOnlyArray<{
-            id : string,
-            label : {
-                description : string
-            }
-        }>
-    },
-    sofort? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    venmo? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    wechatpay? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    zimpler? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    itau? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    payu? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    verkkopankki? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    blik? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    boleto? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    maxima? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    oxxo? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    },
-    trustly? : {
-        eligible : boolean,
-        branded : boolean,
-        recommended? : ?boolean
-    }
+    paypal? : PayPalEligibility,
+    card : CardEligibility,
+    venmo? : BasicEligibility,
+    credit? : BasicEligibility,
+    sepa? : BasicEligibility,
+    bancontact? : BasicEligibility,
+    eps? : BasicEligibility,
+    giropay? : BasicEligibility,
+    ideal? : BasicEligibility,
+    mybank? : BasicEligibility,
+    p24? : BasicEligibility,
+    sofort? : BasicEligibility,
+    wechatpay? : BasicEligibility,
+    zimpler? : BasicEligibility,
+    itau? : BasicEligibility,
+    payu? : BasicEligibility,
+    verkkopankki? : BasicEligibility,
+    blik? : BasicEligibility,
+    boleto? : BasicEligibility,
+    maxima? : BasicEligibility,
+    oxxo? : BasicEligibility,
+    trustly? : BasicEligibility
 |};
