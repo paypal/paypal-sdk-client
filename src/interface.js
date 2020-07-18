de71daa347fc134445347396e7a139996a7d783e
Daniel Brain <daniel@bluesuncorp.co.uk>
2020-07-18 12:33:21 -0700

    Export getCorrelationID helper

--- src/interface.js
+++ src/interface.js
@@ -5,6 +5,7 @@ import { getSDKScript } from './script';
 import { checkForCommonErrors } from './errors';
 
 export { FUNDING } from '@paypal/sdk-constants/src';
+export { getCorrelationID } from './globals';
 
 export function setup() {
     checkForCommonErrors();
