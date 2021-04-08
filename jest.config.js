b6487d630c0704046658eab604b1d9b63cc7bd57
Gabriel Ortega <gaortega@paypal.com>
2021-04-08 18:22:00 -0500

    lint fixed

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
-import { sdkClientTestGlobals } from "./test/globals";
+/* @flow */
+import { sdkClientTestGlobals } from './test/globals';
 
+/* eslint import/no-commonjs: off */
 module.exports = {
-  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
-  globals: {
-    ...sdkClientTestGlobals,
-    __ENV__: "test",
-    __DEBUG__: false,
-    __TEST__: true,
-    __WEB__: false
-  }
+    transformIgnorePatterns: [ 'node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)' ],
+    globals:                 {
+        ...sdkClientTestGlobals,
+        __ENV__:   'test',
+        __DEBUG__: false,
+        __TEST__:  true,
+        __WEB__:   false
+    }
 };
--- test/jest/domain.test.js
+++ test/jest/domain.test.js
@@ -1,99 +1,100 @@
+/* @flow */
 import {
-  getPayPalDomain,
-  getPayPalAPIDomain,
-  insertMockSDKScript
-} from "../../src";
+    getPayPalDomain,
+    getPayPalAPIDomain,
+    insertMockSDKScript
+} from '../../src';
 
 beforeEach(() => {
-  __ENV__ = "test";
+    __ENV__ = 'test';
 });
 
 describe(`getPayPalDomain function test cases`, () => {
-  it("should successfully get the production paypal domain", async () => {
-    __ENV__ = "production";
-    const expectedPayPalDomain = "https://www.paypal.com";
-    expect(getPayPalDomain()).toBe(expectedPayPalDomain);
-  });
-
-  it("should successfully get the stage paypal domain", async () => {
-    __ENV__ = "stage";
-    const expectedPayPalDomain = "https://msmaster.qa.paypal.com";
-    expect(getPayPalDomain()).toBe(expectedPayPalDomain);
-  });
-
-  it("should successfully get the sandbox paypal domain", async () => {
-    const expectedPayPalDomain = "https://www.sandbox.paypal.com";
-    __ENV__ = "sandbox";
-
-    insertMockSDKScript({
-      query: {
-        "client-id": "sb"
-      }
+    it('should successfully get the production paypal domain', () => {
+        __ENV__ = 'production';
+        const expectedPayPalDomain = 'https://www.paypal.com';
+        expect(getPayPalDomain()).toBe(expectedPayPalDomain);
     });
 
-    expect(getPayPalDomain()).toBe(expectedPayPalDomain);
-  });
-
-  it("should successfully get the sandbox v2 paypal domain", async () => {
-    const expectedPayPalDomain = "https://www-v2.sandbox.paypal.com";
-    __ENV__ = "sandbox";
-
-    insertMockSDKScript({
-      query: {
-        "client-id": "sb",
-        env: "sandbox_v2"
-      }
+    it('should successfully get the stage paypal domain', () => {
+        __ENV__ = 'stage';
+        const expectedPayPalDomain = 'https://msmaster.qa.paypal.com';
+        expect(getPayPalDomain()).toBe(expectedPayPalDomain);
     });
 
-    expect(getPayPalDomain()).toBe(expectedPayPalDomain);
-  });
+    it('should successfully get the sandbox paypal domain', () => {
+        const expectedPayPalDomain = 'https://www.sandbox.paypal.com';
+        __ENV__ = 'sandbox';
 
-  it("should thrown error if can't find the requested domain", async () => {
-    __ENV__ = "foo";
-    expect(() => getPayPalDomain()).toThrowError(
-      /Can not get paypal domain for env/
-    );
-  });
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb'
+            }
+        });
+
+        expect(getPayPalDomain()).toBe(expectedPayPalDomain);
+    });
+
+    it('should successfully get the sandbox v2 paypal domain', () => {
+        const expectedPayPalDomain = 'https://www-v2.sandbox.paypal.com';
+        __ENV__ = 'sandbox';
+
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb',
+                'env':         'sandbox_v2'
+            }
+        });
+
+        expect(getPayPalDomain()).toBe(expectedPayPalDomain);
+    });
+
+    it('should thrown error if can\'t find the requested domain', () => {
+        __ENV__ = 'foo';
+        expect(() => getPayPalDomain()).toThrowError(
+            /Can not get paypal domain for env/
+        );
+    });
 });
 
 describe(`getPayPalAPIDomain function test cases`, () => {
-  it("should successfully get the production paypal API domain", async () => {
-    __ENV__ = "production";
-    const expectedPayPalDomain = "https://cors.api.paypal.com";
-    expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
-  });
-
-  it("should successfully get the sandbox paypal API domain", async () => {
-    const expectedPayPalDomain = "https://cors.api.sandbox.paypal.com";
-    __ENV__ = "sandbox";
-
-    insertMockSDKScript({
-      query: {
-        "client-id": "sb"
-      }
+    it('should successfully get the production paypal API domain', () => {
+        __ENV__ = 'production';
+        const expectedPayPalDomain = 'https://cors.api.paypal.com';
+        expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
     });
 
-    expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
-  });
+    it('should successfully get the sandbox paypal API domain', () => {
+        const expectedPayPalDomain = 'https://cors.api.sandbox.paypal.com';
+        __ENV__ = 'sandbox';
 
-  it("should successfully get the sandbox v2 paypal API domain", async () => {
-    const expectedPayPalDomain = "https://cors.api-v2.sandbox.paypal.com";
-    __ENV__ = "sandbox";
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb'
+            }
+        });
 
-    insertMockSDKScript({
-      query: {
-        "client-id": "sb",
-        env: "sandbox_v2"
-      }
+        expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
     });
 
-    expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
-  });
+    it('should successfully get the sandbox v2 paypal API domain', () => {
+        const expectedPayPalDomain = 'https://cors.api-v2.sandbox.paypal.com';
+        __ENV__ = 'sandbox';
 
-  it("should thrown error if can't find the requested API domain", async () => {
-    __ENV__ = "foo";
-    expect(() => getPayPalAPIDomain()).toThrowError(
-      /Can not get paypal api domain for env/
-    );
-  });
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb',
+                'env':         'sandbox_v2'
+            }
+        });
+
+        expect(getPayPalAPIDomain()).toBe(expectedPayPalDomain);
+    });
+
+    it('should thrown error if can\'t find the requested API domain', () => {
+        __ENV__ = 'foo';
+        expect(() => getPayPalAPIDomain()).toThrowError(
+            /Can not get paypal api domain for env/
+        );
+    });
 });
