34aa56319616959e59f6c9abc36db92e8719721a
Gabriel Ortega <gaortega@paypal.com>
2021-04-09 17:06:28 -0500

    test added with standard approach

--- test/client/config.js
+++ test/client/config.js
@@ -1,10 +1,14 @@
 /* @flow */
 
-import { getPayPalDomain, getPayPalAPIDomain, getPayPalLoggerDomain, buildPayPalUrl, buildPayPalAPIUrl, getPayPalLoggerUrl } from '../../src';
+import { insertMockSDKScript, getPayPalDomain, getPayPalAPIDomain, getPayPalLoggerDomain, buildPayPalUrl, buildPayPalAPIUrl, getPayPalLoggerUrl } from '../../src';
+
+beforeEach(() => {
+    window.__ENV__ = 'test';
+});
 
 describe(`config cases`, () => {
 
-    it('should successfully get the paypal domain', () => {
+    it('should successfully get the testing paypal domain', () => {
         const expectedPayPalDomain = 'mock://www.paypal.com';
 
         if (getPayPalDomain() !== expectedPayPalDomain) {
@@ -12,11 +16,115 @@ describe(`config cases`, () => {
         }
     });
 
-    it('should successfully get the paypal api domain', () => {
+    it('should successfully get the production paypal domain', () => {
+        window.__ENV__ = 'production';
+        const expectedPayPalDomain = 'https://www.paypal.com';
+
+        if (getPayPalDomain() !== expectedPayPalDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalDomain }, got ${ getPayPalDomain() }`);
+        }
+    });
+
+    it('should successfully get the sandbox paypal domain', () => {
+        const expectedPayPalDomain = 'https://www.sandbox.paypal.com';
+        window.__ENV__ = 'sandbox';
+
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb'
+            }
+        });
+
+        if (getPayPalDomain() !== expectedPayPalDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalDomain }, got ${ getPayPalDomain() }`);
+        }
+    });
+
+    it('should successfully get the sandbox v2 paypal domain', () => {
+        const expectedPayPalDomain = 'https://www-v2.sandbox.paypal.com';
+        window.__ENV__ = 'sandbox';
+
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb',
+                'env':         'sandbox_v2'
+            }
+        });
+
+        if (getPayPalDomain() !== expectedPayPalDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalDomain }, got ${ getPayPalDomain() }`);
+        }
+    });
+
+    it('should thrown error if can\'t find the requested domain', () => {
+        window.__ENV__ = 'foo';
+
+        try {
+            getPayPalDomain();
+        } catch (error) {
+            if (!error) {
+                throw new Error(`Expected paypal domain to thrown error for domain ${ __ENV__ }`);
+            }
+        }
+    });
+
+    it('should successfully get the testing paypal api domain', () => {
         const expectedPayPalAPIDomain = 'mock://api.paypal.com';
 
         if (getPayPalAPIDomain() !== expectedPayPalAPIDomain) {
             throw new Error(`Expected paypal api domain to be ${ expectedPayPalAPIDomain }, got ${ getPayPalAPIDomain() }`);
+        }
+    });
+
+    it('should successfully get the sandbox paypal API domain', () => {
+        const expectedPayPalAPIDomain = 'https://cors.api.sandbox.paypal.com';
+        window.__ENV__ = 'sandbox';
+
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb'
+            }
+        });
+
+        if (getPayPalAPIDomain() !== expectedPayPalAPIDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalAPIDomain }, got ${ getPayPalAPIDomain() }`);
+        }
+    });
+
+    it('should successfully get the sandbox v2 paypal API domain', () => {
+        const expectedPayPalAPIDomain = 'https://cors.api-v2.sandbox.paypal.com';
+        window.__ENV__ = 'sandbox';
+
+        insertMockSDKScript({
+            query: {
+                'client-id': 'sb',
+                'env':         'sandbox_v2'
+            }
+        });
+
+        if (getPayPalAPIDomain() !== expectedPayPalAPIDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalAPIDomain }, got ${ getPayPalAPIDomain() }`);
+        }
+    });
+
+    it('should successfully get the production paypal API domain', () => {
+        window.__ENV__ = 'production';
+        const expectedPayPalAPIDomain = 'https://cors.api.paypal.com';
+
+        if (getPayPalAPIDomain() !== expectedPayPalAPIDomain) {
+            throw new Error(`Expected paypal domain to be ${ expectedPayPalAPIDomain }, got ${ getPayPalAPIDomain() }`);
+        }
+    });
+
+    it('should thrown error if can\'t find the requested API domain', () => {
+        window.__ENV__ = 'foo';
+
+        try {
+            getPayPalAPIDomain();
+        } catch (error) {
+            if (!error) {
+                throw new Error(`Expected paypal domain to thrown error for domain ${ __ENV__ }`);
+            }
         }
     });
 
