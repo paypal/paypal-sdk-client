d93df03a5fd9a7905523559446a45a4239db8aa0
Amy Egan <amyegan@paypal.com>
2021-05-10 16:25:51 -0400

    recast country and lang at start of loop

--- src/script.js
+++ src/script.js
@@ -217,25 +217,19 @@ export function getLocale() : LocaleType {
         return { lang, country };
     }
 
-    for (const { country, lang } of getBrowserLocales()) {
-        // $FlowFixMe
-        if (COUNTRY_LANGS.hasOwnProperty(country) && COUNTRY_LANGS[country].indexOf(lang) !== -1) {
-            // $FlowFixMe
+    for (let { country, lang } of getBrowserLocales()) {
+        country = country && COUNTRY[country];
+        lang = lang && LANG[lang.toUpperCase()];
+
+        if (country && lang && COUNTRY_LANGS[country] && COUNTRY_LANGS[country].indexOf(lang) !== -1) {
             return { country, lang };
-        } else {
+        } else if (lang) {
             // We infer country from language if there is only one possible country match
-            const possibleCountries = Object.keys(COUNTRY_LANGS).filter(x => COUNTRY_LANGS[x].some(y => y === lang));
+            const possibleCountries = Object.keys(COUNTRY_LANGS).filter(c => COUNTRY_LANGS[c].some(l => l === lang));
             
-            if (possibleCountries.length !== 1) {
-                break;
-            }
-
-            const possibleCountry = possibleCountries[0];
-            const languages = COUNTRY_LANGS[possibleCountry];
-            const language = languages.find(x => x === lang);
-
-            if (language) {
-                return { country: possibleCountry, lang: language };
+            if (possibleCountries.length === 1) {
+                const possibleCountry = possibleCountries[0];
+                return { country: possibleCountry, lang };
             }
         }
     }
