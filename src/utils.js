61a8c10e7fb573cca7c1cee1ba7427346b4a3bec
Gabriel Ortega <gaortega@paypal.com>
2021-11-01 15:33:28 -0500

    fix flow warnings

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,17 @@
 /* @flow */
 import type { LocaleType } from '@paypal/sdk-constants/src/';
-import { COUNTRY_LANGS, LANG } from '@paypal/sdk-constants/src';
+import { COUNTRY_LANGS, LANG, COUNTRY } from '@paypal/sdk-constants/src';
 
 export function getComputedLocales(locale : string) : LocaleType {
-    let [ lang, country ] = locale.split('_');
-    // $FlowFixMe
+    const [ _lang, _country ] = locale.split('_');
+    
+    let lang = LANG[_lang.toUpperCase()];
+    const country = COUNTRY[_country];
     const countryLangs = COUNTRY_LANGS[country];
 
     if (countryLangs.includes(LANG.ZH_HANT)) {
         lang = LANG.ZH_HANT;
     }
 
-    // $FlowFixMe
     return { lang, country };
 }
