24f676f828620c9e3354e5829af0053d211b888b
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-11-17 10:34:31 -0600

    fix: replace includes() with indexOf() (#92)

--- src/utils.js
+++ src/utils.js
@@ -4,12 +4,12 @@ import { COUNTRY_LANGS, LANG, COUNTRY, type LocaleType } from '@paypal/sdk-const
 
 export function getComputedLocales(locale : string) : LocaleType {
     let [ lang, country ] = locale.split('_');
-    
+
     lang = LANG[lang.toUpperCase()];
     country = COUNTRY[country];
     const countryLangs = COUNTRY_LANGS[country];
 
-    if (countryLangs && countryLangs.includes(LANG.ZH_HANT) && lang === LANG.ZH) {
+    if (countryLangs && countryLangs.indexOf(LANG.ZH_HANT) !== -1 && lang === LANG.ZH) {
         lang = LANG.ZH_HANT;
     }
 
