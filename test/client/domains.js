555f5f886e0e1c85602efb6b4725785350167fb3
Arturo Riveron Borodovisina <aborodovisina@paypal.com>
2022-03-04 15:28:50 +0100

    test(domain): increase unit test coverage

--- test/client/domains.js
+++ test/client/domains.js
@@ -1,10 +1,17 @@
 /* @flow */
+import { ENV } from '@paypal/sdk-constants/src';
 
 import { getPayPalDomainRegex } from '../../src';
+import {
+    getPayPalLoggerDomain,
+    getAuthAPIUrl,
+    getOrderAPIUrl
+} from '../../src/domains';
 
 beforeEach(() => {
     window.__ENV__ = 'test';
 });
+
 describe(`domains test`, () => {
     it('should successfully match valid domain', () => {
 
@@ -36,6 +43,46 @@ describe(`domains test`, () => {
             if (domain.match(getPayPalDomainRegex())) {
                 throw new Error(`${ domain } must not match the regex`);
             }
+        }
+    });
+
+    it('getPayPalLoggerDomain should return the logger domain when is a local environment', () => {
+        window.__ENV__ = ENV.LOCAL;
+        const result = getPayPalLoggerDomain();
+
+        if (result !== 'https://mock://msmaster.qa.paypal.com') {
+            throw new Error(`should get the logger domain "https://mock://msmaster.qa.paypal.com", but got: ${ result }`);
+        }
+    });
+
+    it('getPayPalLoggerDomain should thrown an Error when is a local environment and the stage host is undefined', () => {
+        window.__ENV__ = ENV.LOCAL;
+        window.__STAGE_HOST__ = undefined;
+        try {
+            getPayPalLoggerDomain();
+        } catch (err) {
+            if (err.message !== 'No stage host found') {
+                throw new Error(`should thrown exception with message "No stage host found", but got ${ err.message }`);
+            }
+        }
+        window.__STAGE_HOST__ = 'mock://msmaster.qa.paypal.com';
+    });
+
+    it('getAuthAPIUrl should return a valid authentication string URL', () => {
+        const url = new URL(getAuthAPIUrl()); // eslint-disable-line compat/compat
+
+        if (`${ url.protocol }//${ url.hostname }` !== 'http://localhost' ||
+            url.pathname !== '/v1/oauth2/token') {
+            throw new Error(`should get the logger domain "${ window.location.protocol }//${ window.location.host }/v1/oauth2/token", but got: ${ url.toString() }`);
+        }
+    });
+
+    it('getOrderAPIUrl should return a valid order string URL', () => {
+        const url = new URL(getOrderAPIUrl()); // eslint-disable-line compat/compat
+
+        if (`${ url.protocol }//${ url.hostname }` !== 'http://localhost' ||
+        url.pathname !== '/v2/checkout/orders') {
+            throw new Error(`should get the logger domain "${ window.location.protocol }//${ window.location.host }/v2/checkout/orders", but got: ${ url.toString() }`);
         }
     });
 });
