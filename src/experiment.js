0afe2477e31adba13b3f138497881a44b62591da
Steve Mask <smask@paypal.com>
2022-06-21 10:13:40 -0400

    add ability to pass logger

--- src/experiment.js
+++ src/experiment.js
@@ -2,13 +2,14 @@
 
 
 import { experiment, type Experiment } from '@krakenjs/belter/src';
+import { type LoggerType } from '@krakenjs/beaver-logger/src';
 import { FPTI_KEY } from '@paypal/sdk-constants/src';
 
 import { FPTI_STATE, FPTI_TRANSITION } from './constants';
 import { getLogger } from './logger';
 
-export function createExperiment(name : string, sample : number) : Experiment {
-    const logger = getLogger();
+export function createExperiment(name : string, sample : number, logger? : LoggerType) : Experiment {
+    const log = logger || getLogger();
 
     return experiment({
         name,
@@ -24,18 +25,18 @@ export function createExperiment(name : string, sample : number) : Experiment {
                 ...payload
             };
 
-            logger.track(fullPayload);
-            logger.flush();
+            log.track(fullPayload);
+            log.flush();
         },
 
         logCheckpoint({ treatment, checkpoint, payload }) {
             if (treatment.indexOf(name) !== -1) {
-                logger.info(`${ treatment }_${ checkpoint }`, payload);
+                log.info(`${ treatment }_${ checkpoint }`, payload);
             } else {
-                logger.info(`${ name }_${ treatment }_${ checkpoint }`, payload);
+                log.info(`${ name }_${ treatment }_${ checkpoint }`, payload);
             }
-            
-            logger.flush();
+
+            log.flush();
         }
     });
 }
