d06a6f86031202be1516a626cdc6ec330e6ad86a
Steven Mask <mnicpt@gmail.com>
2023-06-12 11:39:45 -0400

    feat(venmo): domain support (#155)
    * feat(venmo): domain support
    
    * popup domain
    
    * revert popup domain
    
    * add test
    
    * tests for local
    
    ---------
    
    Co-authored-by: Steve Mask <smask@paypal.com>

--- test/client/domains.js
+++ test/client/domains.js
@@ -1,7 +1,11 @@
 /* @flow */
 import { ENV } from "@paypal/sdk-constants/src";
 
-import { getPayPalDomainRegex } from "../../src";
+import {
+  getPayPalDomainRegex,
+  getVenmoDomainRegex,
+  isPayPalTrustedDomain,
+} from "../../src";
 import {
   getPayPalLoggerDomain,
   getAuthAPIUrl,
@@ -13,7 +17,7 @@ beforeEach(() => {
 });
 
 describe(`domains test`, () => {
-  it("should successfully match valid domain", () => {
+  it("should successfully match valid paypal domain", () => {
     const validDomains = [
       "master.qa.paypal.com",
       "test-env.qa.paypal.com:3000",
@@ -32,7 +36,7 @@ describe(`domains test`, () => {
     }
   });
 
-  it("should not match invalid domains", () => {
+  it("should not match invalid paypal domains", () => {
     const invalidDomains = [
       "www.paypal.com.example.com",
       "www.paypal.cn.example.com",
@@ -42,6 +46,56 @@ describe(`domains test`, () => {
       if (domain.match(getPayPalDomainRegex())) {
         throw new Error(`${domain} must not match the regex`);
       }
+    }
+  });
+
+  it("should successfully match valid venmo domain", () => {
+    const validDomains = [
+      "www.venmo.com",
+      "www.venmo.com:8000",
+      "account.qa.venmo.com",
+      "www.account.qa.venmo.com",
+      "venmo.com",
+      "id.venmo.com",
+    ];
+
+    for (const domain of validDomains) {
+      if (!domain.match(getVenmoDomainRegex())) {
+        throw new Error(`${domain} must match the regex`);
+      }
+    }
+  });
+
+  it("should successfully match valid venmo testing domain", () => {
+    window.__ENV__ = "local";
+    const validDomains = ["localhost.venmo.com"];
+
+    for (const domain of validDomains) {
+      if (!domain.match(getVenmoDomainRegex())) {
+        throw new Error(`${domain} must match the regex`);
+      }
+    }
+  });
+
+  it("should not match invalid venmo domains", () => {
+    const invalidDomains = [
+      "www.venmo.com.example.com",
+      "www.venmo.cn.example.com",
+    ];
+
+    for (const domain of invalidDomains) {
+      if (domain.match(getVenmoDomainRegex())) {
+        throw new Error(`${domain} must not match the regex`);
+      }
+    }
+  });
+
+  it("isPayPalTrustedDomain should return true", () => {
+    window.__ENV__ = ENV.LOCAL;
+    const result = isPayPalTrustedDomain();
+
+    if (!result) {
+      throw new Error("should get true, but got false");
     }
   });
 
