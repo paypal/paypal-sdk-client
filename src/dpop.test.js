569d30f02c772274f37fd0a087a8e5e71fe9c6c6
Jesse Shawl <jshawl@paypal.com>
2024-02-15 09:13:59 -0600

    add dpop encoding utils
    alphabetize import and clarify base64 test case
    
    add keypair generation and thumbprint confirmation

--- src/dpop.test.js
+++ src/dpop.test.js
@@ -6,6 +6,8 @@ import {
   base64decodeUrlSafe,
   base64encodeUrlSafe,
   bytesToString,
+  generateKeyPair,
+  jsonWebKeyThumbprint,
   sha256,
   stringToBytes,
 } from "./dpop";
@@ -42,6 +44,32 @@ describe("DPoP", () => {
       );
       expect(digest).toEqual("fUHyO2r2Z3DZ53EsNrWBb0xWXoaNy59IiKCAqksmQEo");
       expect.assertions(1);
+    });
+  });
+  describe("keypair generation", () => {
+    it("memoizes the keypair", async () => {
+      const { publicKey: publicKey1 } = await generateKeyPair();
+      const jwk1 = await window.crypto.subtle.exportKey("jwk", publicKey1);
+      const { publicKey: publicKey2 } = await generateKeyPair();
+      const jwk2 = await window.crypto.subtle.exportKey("jwk", publicKey2);
+      expect(jwk1.x).toBeTruthy();
+      expect(jwk1).toStrictEqual(jwk2);
+    });
+  });
+  describe("JSON Web Key Thumbprint", () => {
+    it("generates a correct thumbprint", async () => {
+      // testing a known JSON Web Key and its thumbprint from:
+      // https://datatracker.ietf.org/doc/html/rfc7638#section-3.1
+      const key = {
+        kty: "RSA",
+        n: "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+        e: "AQAB",
+        alg: "RS256",
+        kid: "2011-04-29",
+      };
+      expect(await jsonWebKeyThumbprint(key)).toBe(
+        "NzbLsXh8uDCcd-6MNwXF4W_7noWXFZAfHkxZsRGC9Xs"
+      );
     });
   });
 });
