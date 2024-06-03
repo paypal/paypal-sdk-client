185aa900a107edfb54c1a5143d6823fbde4b2d6d
Nikolaus Roman <nikrom17@gmail.com>
2024-06-03 10:36:38 -0500

    add cleanup comments

--- src/types.js
+++ src/types.js
@@ -4,6 +4,7 @@ export const _TYPES = true;
 
 export * from "@paypal/sdk-constants/src/types";
 
+// TODO first-render-experiment-cleanup
 export type Experimentation = {
   experience?: string,
   treatment?: string,
@@ -13,6 +14,7 @@ export type Experimentation = {
   [any]: empty,
 };
 
+// TODO first-render-experiment-cleanup
 export type GetExperimentation = {
   __EXPERIENCE__?: string,
   __TREATMENT__?: string,
