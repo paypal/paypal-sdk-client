cbc600c08ef9f850ae5e3d1ec1c2d45b63fa780e
Shane Brunson <brunson.ws@gmail.com>
2024-06-07 14:04:21 -0500

    remove logging test

--- src/tracking.test.js
+++ src/tracking.test.js
@@ -1,5 +1,5 @@
 /* @flow */
-/* eslint-disable promise/no-native, no-restricted-globals, compat/compat */
+
 import { describe, it, vi, expect } from "vitest";
 
 import { makeMockScriptElement } from "../test/helpers";
@@ -9,17 +9,12 @@ import { getSDKInitTime, setupLogger } from "./tracking";
 const clientId = "foobar123";
 const mockScriptSrc = `https://test.paypal.com/sdk/js?client-id=${clientId}`;
 
-let metricCounterSpy;
-let windowReadyPromiseResolver;
 vi.mock("./logger", async () => {
   const actual = await vi.importActual("./logger");
   return {
     ...actual,
     getLogger: vi.fn(() => {
       const logger = actual.getLogger();
-      metricCounterSpy = vi
-        .spyOn(logger, "metricCounter")
-        .mockImplementation(() => {}); // eslint-disable-line no-empty-function
       return logger;
     }),
   };
@@ -31,11 +26,6 @@ vi.mock("@krakenjs/belter/src", async () => {
     ...actual,
     getCurrentScript: vi.fn(() => {
       return makeMockScriptElement(mockScriptSrc);
-    }),
-    waitForWindowReady: vi.fn(() => {
-      return new Promise((resolve) => {
-        windowReadyPromiseResolver = resolve;
-      });
     }),
   };
 });
@@ -50,25 +40,4 @@ describe(`tracking cases`, () => {
     setupLogger();
     expect(getSDKInitTime()).toEqual(expect.any(Number));
   });
-
-  it("setupLogger should setup logger with IEIntranet enabled", () => {
-    window.document.documentMode = true;
-    setupLogger();
-  });
-
-  it("should call metric counter on window load", async () => {
-    windowReadyPromiseResolver();
-    await new Promise((resolve) => setTimeout(resolve)); // Flush promises
-
-    expect(metricCounterSpy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        namespace: "sdk_client.init.count",
-        dimensions: expect.objectContaining({
-          isPayPalDomain: "false",
-        }),
-      })
-    );
-  });
 });
-
-/* eslint-enable promise/no-native, no-restricted-globals, compat/compat */
